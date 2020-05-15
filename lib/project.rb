5e0e031dbffb378b54c01704e4913d07523d8988
Joseph Romero <joeromero47@gmail.com>
2020-05-15 02:07:34 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.backed_projects(self) unless backer.backed_projects.include? self
+    add_backer.backer(self) unless backer.backed_projects.include? self
   end
 end
